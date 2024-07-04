6fe071f36c6610921635f17f4c0c319d59a05610
AZarbade <adwaitzarbade@gmail.com>
2024-07-04 23:52:29 +0530

    base functionalty works

--- lua/cmc/config.lua
+++ lua/cmc/config.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.setup()
-	vim.api.nvim_create_user_command("CMCHello", function()
-		require("cmc").hello()
+	vim.api.nvim_create_user_command("CompileC", function()
+		require("cmc").compile_c_file()
 	end, {})
 
-	vim.keymap.set("n", "<leader>ch", require("cmc").hello, { noremap = true, silent = false })
+	vim.keymap.set("n", "<F5>", require("cmc").compile_c_file, { noremap = true, silent = false })
 end
 
 return M
--- lua/cmc/init.lua
+++ lua/cmc/init.lua
@@ -1,7 +1,17 @@
 local M = {}
 
-M.hello = function()
-	print("Hello from cmc.nvim!")
+M.compile_c_file = function()
+	local filename = vim.fn.expand("%:t")
+	local command = string.format("gcc %s -o main -g", filename)
+
+	local output = vim.fn.system(command)
+
+	if vim.v.shell_error == 0 then
+		print("Compilation successful!")
+	else
+		print("Compilation failed. Error message:")
+		print(output)
+	end
 end
 
 return M
