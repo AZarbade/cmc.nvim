6ae0be4a99b3fed559d6371712e7dbaa8f76067b
AZarbade <adwaitzarbade@gmail.com>
2024-07-05 00:20:34 +0530

    seperate compile with debug

--- lua/cmc/config.lua
+++ lua/cmc/config.lua
@@ -2,10 +2,10 @@ local M = {}
 
 function M.setup()
 	vim.api.nvim_create_user_command("CompileC", function()
-		require("cmc").compile_c_file()
+		require("cmc")
 	end, {})
 
-	vim.keymap.set("n", "<F5>", require("cmc").compile_c_file, { noremap = true, silent = false })
+	vim.keymap.set("n", "<F5>", require("cmc").compile_c, { noremap = true, silent = false })
 end
 
 return M
--- lua/cmc/init.lua
+++ lua/cmc/init.lua
@@ -1,9 +1,22 @@
 local M = {}
 
-M.compile_c_file = function()
-	local filename = vim.fn.expand("%:t")
-	local command = string.format("gcc %s -o main -g", filename)
+local out_name = "main"
+local filename = vim.fn.expand("%:t")
 
+M.compile_c = function()
+	local command = string.format("gcc %s -o %s", filename, out_name)
+	local output = vim.fn.system(command)
+
+	if vim.v.shell_error == 0 then
+		print("Compilation successful!")
+	else
+		print("Compilation failed. Error message:")
+		print(output)
+	end
+end
+
+M.compile_c_debug = function()
+	local command = string.format("gcc %s -o %s -g", filename, out_name)
 	local output = vim.fn.system(command)
 
 	if vim.v.shell_error == 0 then
