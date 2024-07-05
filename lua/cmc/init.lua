0c36ea95119231d1198b148fc9c2436de097a4e3
AZarbade <adwaitzarbade@gmail.com>
2024-07-05 16:46:26 +0530

    fix: compile error displayed

--- lua/cmc/init.lua
+++ lua/cmc/init.lua
@@ -16,25 +16,26 @@ end
 local function compile(debug)
 	local filename = vim.fn.expand("%:t")
 	if filename == "" then
-		print("No file open")
+		vim.api.nvim_err_writeln("No file open")
 		return
 	end
 
 	if not filename:match("%.c$") then
-		print("Not a C file")
+		vim.api.nvim_err_writeln("Not a C file")
 		return
 	end
 
 	local out_name = get_output_name()
 	local debug_flag = debug and " -g" or ""
 	local command = string.format("gcc %s -o %s%s", filename, out_name, debug_flag)
+
+	vim.o.errorformat = [[%f:%l:%c: %t%*[^:]: %m]]
 	local output = vim.fn.system(command)
 
 	if vim.v.shell_error == 0 then
-		print("Compilation successful! Output: " .. out_name)
+		vim.api.nvim_echo({ { "Compilation successful! Output: " .. out_name, "Normal" } }, true, {})
 	else
-		print("Compilation failed. Error message:")
-		print(output)
+		vim.api.nvim_err_writeln("Compilation failed. See quickfix list for details.")
 	end
 end
 
