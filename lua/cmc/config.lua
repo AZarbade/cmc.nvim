cf54f01e5789dce47034bad08bb991045b4b62af
AZarbade <adwaitzarbade@gmail.com>
2024-07-05 16:59:29 +0530

    Removed quickfix list integration

--- lua/cmc/config.lua
+++ lua/cmc/config.lua
@@ -1,27 +1,14 @@
 local M = {}
-
 function M.setup(opts)
 	require("cmc").setup(opts)
-
 	vim.api.nvim_create_user_command("CompileC", function()
-		vim.fn.setqflist({}, "r") -- Clear the quickfix list
 		require("cmc").compile_c()
 	end, {})
-
 	vim.api.nvim_create_user_command("CompileCDebug", function()
-		vim.fn.setqflist({}, "r") -- Clear the quickfix list
 		require("cmc").compile_c_debug()
 	end, {})
 
-	vim.keymap.set("n", "<leader>cc", function()
-		vim.fn.setqflist({}, "r") -- Clear the quickfix list
-		require("cmc").compile_c()
-	end, { noremap = true, silent = false })
-
-	vim.keymap.set("n", "<leader>cd", function()
-		vim.fn.setqflist({}, "r") -- Clear the quickfix list
-		require("cmc").compile_c_debug()
-	end, { noremap = true, silent = false })
+	vim.keymap.set("n", "<leader>cc", require("cmc").compile_c, { noremap = true, silent = false })
+	vim.keymap.set("n", "<leader>cd", require("cmc").compile_c_debug, { noremap = true, silent = false })
 end
-
 return M
