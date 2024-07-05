c850b3b9ca0ce05649a51f5816f0702bcabd05cc
AZarbade <adwaitzarbade@gmail.com>
2024-07-05 12:39:20 +0530

    some refactoring and feat. add - added config table. - seperate function for w/ and w/o debug. - custom output name from user.

--- lua/cmc/config.lua
+++ lua/cmc/config.lua
@@ -1,11 +1,18 @@
 local M = {}
 
-function M.setup()
+function M.setup(opts)
+	require("cmc").setup(opts)
+
 	vim.api.nvim_create_user_command("CompileC", function()
-		require("cmc")
+		require("cmc").compile_c()
 	end, {})
 
-	vim.keymap.set("n", "<F5>", require("cmc").compile_c, { noremap = true, silent = false })
+	vim.api.nvim_create_user_command("CompileCDebug", function()
+		require("cmc").compile_c_debug()
+	end, {})
+
+	vim.keymap.set("n", "<leader>cc", require("cmc").compile_c, { noremap = true, silent = false })
+	vim.keymap.set("n", "<leader>cd", require("cmc").compile_c_debug, { noremap = true, silent = false })
 end
 
 return M
--- lua/cmc/init.lua
+++ lua/cmc/init.lua
@@ -1,30 +1,53 @@
 local M = {}
 
-local out_name = "main"
-local filename = vim.fn.expand("%:t")
+local config = {
+	output_name = "main",
+	ask_output_name = false,
+}
 
-M.compile_c = function()
-	local command = string.format("gcc %s -o %s", filename, out_name)
+local function get_output_name()
+	if config.ask_output_name then
+		local input = vim.fn.input("Enter output name: ")
+		return input ~= "" and input or config.output_name
+	end
+	return config.output_name
+end
+
+local function compile(debug)
+	local filename = vim.fn.expand("%:t")
+	if filename == "" then
+		print("No file open")
+		return
+	end
+
+	if not filename:match("%.c$") then
+		print("Not a C file")
+		return
+	end
+
+	local out_name = get_output_name()
+	local debug_flag = debug and " -g" or ""
+	local command = string.format("gcc %s -o %s%s", filename, out_name, debug_flag)
 	local output = vim.fn.system(command)
 
 	if vim.v.shell_error == 0 then
-		print("Compilation successful!")
+		print("Compilation successful! Output: " .. out_name)
 	else
 		print("Compilation failed. Error message:")
 		print(output)
 	end
 end
 
-M.compile_c_debug = function()
-	local command = string.format("gcc %s -o %s -g", filename, out_name)
-	local output = vim.fn.system(command)
+M.compile_c = function()
+	compile(false)
+end
 
-	if vim.v.shell_error == 0 then
-		print("Compilation successful!")
-	else
-		print("Compilation failed. Error message:")
-		print(output)
-	end
+M.compile_c_debug = function()
+	compile(true)
+end
+
+M.setup = function(opts)
+	config = vim.tbl_deep_extend("force", config, opts or {})
 end
 
 return M
