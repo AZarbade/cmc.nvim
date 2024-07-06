d28a58b2691fa828fa0e5ebd5138de8de3950a7a
AZarbade <adwaitzarbade@gmail.com>
2024-07-06 19:31:41 +0530

    ocd

--- lua/cmc/config.lua
+++ lua/cmc/config.lua
@@ -5,6 +5,7 @@ function M.setup(opts)
 	vim.api.nvim_create_user_command("CompileC", function(cmd)
 		require("cmc").compile(cmd.args)
 	end, { nargs = "*" })
+
 	vim.keymap.set("n", "<leader>cc", function()
 		vim.ui.input({ prompt = "Compiler flags: " }, function(input)
 			require("cmc").compile(input or "")
