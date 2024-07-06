76389429839226ce13a4c1e70e00841818b7b7d2
AZarbade <adwaitzarbade@gmail.com>
2024-07-06 19:16:57 +0530

    things are working

--- lua/cmc/init.lua
+++ lua/cmc/init.lua
@@ -1,57 +1,43 @@
 local M = {}
-
 local config = {
 	output_name = "main",
 	ask_output_name = false,
+	default_flags = "",
 }
 
-local function get_output_name()
-	if config.ask_output_name then
-		local input = vim.fn.input("Enter output name: ")
-		return input ~= "" and input or config.output_name
-	end
-	return config.output_name
-end
-
-local function compile(debug)
+local function compile(args)
 	local filename = vim.fn.expand("%:t")
 	if filename == "" then
 		vim.api.nvim_err_writeln("No file open")
 		return
 	end
-
 	if not filename:match("%.c$") then
 		vim.api.nvim_err_writeln("Not a C file")
 		return
 	end
 
-	local out_name = get_output_name()
-	local command
-	if debug then
-		command = string.format("gcc %s -o %s -g", filename, out_name)
-	else
-		command = string.format("gcc %s -o %s", filename, out_name)
+	local out_name = config.output_name
+	if config.ask_output_name then
+		local input = vim.fn.input("Enter output name: ")
+		out_name = input ~= "" and input or config.output_name
 	end
 
+	local user_args = args or ""
+	local command = string.format("gcc %s %s %s -o %s", user_args, config.default_flags, filename, out_name)
+	print("Debug: Command = " .. command) -- Debug line
+
 	vim.api.nvim_echo({ { "Compiling...", "Normal" } }, false, {})
-
 	local output = vim.fn.system(command)
-
-	vim.api.nvim_command("echo ''")
 
 	if vim.v.shell_error == 0 then
 		vim.api.nvim_echo({ { "Compilation successful! Output: " .. out_name, "Normal" } }, true, {})
 	else
-		vim.api.nvim_err_writeln("Compilation failed. See quickfix list for details.")
+		vim.api.nvim_err_writeln("Compilation failed. Error: " .. output)
 	end
 end
 
-M.compile_c = function()
-	compile(false)
-end
-
-M.compile_c_debug = function()
-	compile(true)
+M.compile = function(args)
+	compile(args)
 end
 
 M.setup = function(opts)
