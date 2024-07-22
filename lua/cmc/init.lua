3b3bdf0ff2487a9c022894cafd3ee656d4521ea4
AZarbade <anzarbade@gmail.com>
2024-07-22 10:20:11 +0530

    significant refactor: - some refactor was needed to makesure user options were properly setup. - different approach for compile_on_save option.

--- lua/cmc/init.lua
+++ lua/cmc/init.lua
@@ -1,12 +1,15 @@
 local M = {}
 
+-- Configuration with default values
 local config = {
-	output_name = "main.out",
-	ask_output_name = false,
-	default_flags = "",
-	compile_on_save = false,
+	output_name = "main.out", -- works
+	ask_output_name = false, -- works
+	default_flags = "", -- works
+	compile_on_save = false, -- works
+	use_makefile = false,
 }
 
+-- Main compilation function
 local function compile(args)
 	local filename = vim.fn.expand("%:t")
 	if filename == "" then
@@ -20,7 +23,7 @@ local function compile(args)
 
 	local out_name = config.output_name
 	if config.ask_output_name then
-		local input = vim.fn.input("Enter output name: ")
+		local input = vim.fn.input("Enter output file name: ")
 		out_name = input ~= "" and input or config.output_name
 	end
 
@@ -28,22 +31,73 @@ local function compile(args)
 	local command = string.format("gcc %s %s %s -o %s", user_args, config.default_flags, filename, out_name)
 
 	vim.api.nvim_echo({ { "Compiling...", "Normal" } }, false, {})
-	local output = vim.fn.system(command)
 
+	local output = vim.fn.system(command)
 	if vim.v.shell_error == 0 then
 		vim.api.nvim_echo({ { "Compilation successful! Output: " .. out_name, "Normal" } }, true, {})
 	else
 		local error = string.format("ERROR: Compilation failed. Error: %s", output)
-		vim.api.nvim_echo({ { error, "ErrorMsg" } }, false, {})
+		vim.api.nvim_err_writeln(error)
 	end
 end
 
-M.compile = function(args)
+-- Public function to trigger compilation
+function M.compile(args)
 	compile(args)
 end
 
-M.setup = function(opts)
+-- Setup function to initialize the plugin
+function M.setup(opts)
 	config = vim.tbl_deep_extend("force", config, opts or {})
+
+	-- Set up autocommand for compile on save if enabled
+	if config.compile_on_save then
+		vim.cmd([[
+            augroup CCompileOnSave
+                autocmd!
+                autocmd BufWritePost *.c lua require'cmc'.compile()
+            augroup END
+        ]])
+	end
+
+	-- Apply default mappings if not disabled
+	if opts.default_mappings ~= false then
+		M.apply_default_mappings()
+	end
+
+	-- Apply user mappings if provided
+	if opts.mappings then
+		M.apply_user_mappings(opts.mappings)
+	end
+end
+
+-- Define default mappings
+M.default_mappings = {
+	compile = "<leader>cc",
+}
+
+function M.apply_default_mappings()
+	for action, key in pairs(M.default_mappings) do
+		vim.api.nvim_set_keymap(
+			"n",
+			key,
+			string.format("<cmd>lua require'cmc'.%s()<CR>", action),
+			{ noremap = true, silent = true }
+		)
+	end
+end
+
+function M.apply_user_mappings(user_mappings)
+	for action, key in pairs(user_mappings) do
+		if M.default_mappings[action] then
+			vim.api.nvim_set_keymap(
+				"n",
+				key,
+				string.format("<cmd>lua require'cmc'.%s()<CR>", action),
+				{ noremap = true, silent = true }
+			)
+		end
+	end
 end
 
 return M
